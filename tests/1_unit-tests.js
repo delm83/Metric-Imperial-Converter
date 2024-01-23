d60382489bd98b4396424327a32852242bca895a
delm83 <delm83@googlemail.com>
2024-01-23 13:52:31 +0000

    more unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -14,6 +14,25 @@ suite('Unit Tests', function(){
         });
 
       test('#equal, #notEqual', function () {
-        assert.equal(1, convertHandler.getNum("kg"), 'no numerical input provided');
+        assert.equal(1, convertHandler.getNum("mi"), 'no numerical input provided');
+        assert.equal("mi", convertHandler.getUnit("50mi"), "unit is mi");
+        assert.equal("km", convertHandler.getUnit("50km"), "unit is km");
+        assert.equal("gal", convertHandler.getUnit("50gal"), "unit is gal");
+        assert.equal("L", convertHandler.getUnit("50L"), "unit is L");
+        assert.equal("lbs", convertHandler.getUnit("50lbs"), "unit is lbs");
+        assert.equal("kg", convertHandler.getUnit("50kg"), "unit is kg");
+        assert.equal("invalid unit", convertHandler.getUnit("50mps"), "unit is invalid");
+        assert.equal("km", convertHandler.getReturnUnit("mi"), "return unit for mi is km");
+        assert.equal("mi", convertHandler.getReturnUnit("km"), "return unit for km is mi");
+        assert.equal("L", convertHandler.getReturnUnit("gal"), "return unit for gal is L");
+        assert.equal("gal", convertHandler.getReturnUnit("L"), "return unit for L is gal");
+        assert.equal("kg", convertHandler.getReturnUnit("lbs"), "return unit for lbs is kg");
+        assert.equal("lbs", convertHandler.getReturnUnit("kg"), "return unit for kg is lbs");
+        assert.equal("miles", convertHandler.spellOutUnit("mi"), "mi is short for miles");
+        assert.equal("kilometres", convertHandler.spellOutUnit("km"), "km is short for kilometres");
+        assert.equal("gallons", convertHandler.spellOutUnit("gal"), "gal is short for gallons");
+        assert.equal("litres", convertHandler.spellOutUnit("L"), "L is short for litres");
+        assert.equal("pounds", convertHandler.spellOutUnit("lbs"), "lbs is short for pounds");
+        assert.equal("kilograms", convertHandler.spellOutUnit("kg"), "kg is short for kilograms");
       });
 });
