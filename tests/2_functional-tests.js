80cec66ed4311998a73c5b9657c1c2e5edae4086
delm83 <delm83@googlemail.com>
2024-01-23 18:43:27 +0000

    begin functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,57 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    this.timeout(5000);
+    suite('Integration tests with chai-http', function () {
+        test('Test GET /convert 10L', function (done) {
+            chai
+              .request(server)
+              .keepOpen()
+              .get('/api/convert?input=10L')
+              .end(function (err, res) {
+                assert.equal(res.status, 200);
+                done();
+              });
+          });
+          test('Test GET /convert 32g', function (done) {
+            chai
+              .request(server)
+              .keepOpen()
+              .get('/api/convert?input=32g')
+              .end(function (err, res) {
+                assert.equal(res.status, 200);
+                done();
+              });
+          });
+          test('Test GET /convert 3/7.2/4kg', function (done) {
+            chai
+              .request(server)
+              .keepOpen()
+              .get('/api/convert?input=3/7.2/4kg')
+              .end(function (err, res) {
+                assert.equal(res.status, 200);
+                done();
+              });
+          });
+          test('Test GET /convert 3/7.2/4kilomegagram', function (done) {
+            chai
+              .request(server)
+              .keepOpen()
+              .get('/api/convert?input=3/7.2/4kilomegagram')
+              .end(function (err, res) {
+                assert.equal(res.status, 200);
+                done();
+              });
+          });
+          test('Test GET /convert kg', function (done) {
+            chai
+              .request(server)
+              .keepOpen()
+              .get('/api/convert?input=kg')
+              .end(function (err, res) {
+                assert.equal(res.status, 200);
+                done();
+              });
+          });
+    });
 });
